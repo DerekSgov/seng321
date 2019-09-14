465dce6ffe675061b46b95542a31896a473c187b
DerekSGov <Derekgsiemens@gmail.com>
2019-09-14 16:50:36 -0700

    Fixing problem statement

--- src/pages/Problem-Statement.js
+++ src/pages/Problem-Statement.js
@@ -10,7 +10,7 @@ const ProblemStatement = () => (
                 <PostContent>
                     <Title>Problem Statement</Title>
                     <PostBody>
-                        <p>At AidConnect, we agknowledge that post-disaster communication and organization can be chaotic and hampering the efforts to help those in need. NGOs and Governmental agencies often miscommunicate and experience issues with providing essential services, such as food and clean water centers, medical assistance and more, with provisions being under or over utilized depending on the location. Due to the natural chaos of the situation and lack of means to access current information on relief efforts, the people these organizations aim to help often struggle to find the aid they require.</p>
+                        <p>At AidConnect, we agknowledge that post-disaster communication and organization can be chaotic and hamper the efforts to help those in need. NGOs and Governmental agencies often miscommunicate and experience issues with providing essential services, such as food and clean water centers, medical assistance and more, with provisions being under or over utilized depending on the location. Due to the natural chaos of the situation and lack of means to access current information on relief efforts, the people these organizations aim to help often struggle to find the aid they require.</p>
                     </PostBody>
                 </PostContent>
             </ArticleContent>
