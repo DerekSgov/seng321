fbf55f9e7d9cd1ee3c78324547da7510e2e20f23
DerekSgov <derekgsiemens@gmail.com>
2019-09-16 19:27:50 -0700

    Adding names and roles of team members

---
+++ src/constants/TeamMembers.js
@@ -0,0 +1,37 @@
+export const TEAM_MEMBERS = [
+    {
+        name: `Chua, Jerusha`,
+        position: `Human Resources Director`,
+        image_url: ``,
+    },
+    {
+        name: `Eng, Andrew`,
+        position: `Chief  Marketing Officer`,
+        image_url: ``,
+    },
+    {
+        name: `Joy, Samuel`,
+        position: `Chief Operations Officer`,
+        image_url: ``,
+    },
+    {
+        name: `Schell, Alex`,
+        position: `Vice President of Sales`,
+        image_url: ``,
+    },
+    {
+        name: `Siemens, Derek`,
+        position: `Chief Legal Officer`,
+        image_url: ``,
+    },
+    {
+        name: `Voorthuyzen, Sho Ya`,
+        position: `Chief Executive Officer`,
+        image_url: ``,
+    },
+    {
+        name: `Yang, Charles`,
+        position: `Chief Financial Officer`,
+        image_url: ``,
+    },
+]
