09136cd8d83dbf6a6764b1afc5b03a1d2e9c8999
DerekSGov <Derekgsiemens@gmail.com>
2019-09-25 11:34:02 -0700

    Adding contact email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => (
                 <section className="post-full-content">
                     <Title>Contact</Title>
                     <h2>Have any questions?</h2>
-                    <p>Feel free to reach us through our contact email on connex!</p>
+                    <p>Feel free to reach us through our contact email: AidConnect@outlook.com</p>
                     <h2>Found an issue with the site?</h2>
                     <p>Make a pull request or create an issue on Github <Link href={`https://github.com/DerekSgov/sen321/issues`}>here</Link></p>
                 </section>
