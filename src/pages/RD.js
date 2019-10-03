f16c17bb5814c766f8f0ca777cc6c401d430b26a
DerekSGov <Derekgsiemens@gmail.com>
2019-10-03 09:41:21 -0700

    changing file name for RD, to RD1.1

--- src/pages/RD.js
+++ src/pages/RD.js
@@ -11,7 +11,7 @@ const RD = () => (
                     <Title>Requirements Document Feedback</Title>
                     <PostBody>
                         <p>A big thanks to Angle Engineering for taking on our RFP!</p>
-                        <p>We enjoyed reading it and look forward to working more with them in the future, click <a href="/images/RD1.0.pdf">here to view our feedback.</a></p>
+                        <p>We enjoyed reading it and look forward to working more with them in the future, click <a href="/images/RD1.1.pdf">here to view our feedback.</a></p>
                     </PostBody>
                 </PostContent>
             </ArticleContent>
